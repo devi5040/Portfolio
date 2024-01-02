440ce204418cfcf309621538aabb0e7a9cc3667a
Deviprasad Rai P <91744010+devi5040@users.noreply.github.com>
2024-01-02 21:03:59 +0530

    Update Header.jsx

--- src/Nav/Header.jsx
+++ src/Nav/Header.jsx
@@ -32,7 +32,7 @@ function Header() {
               : "hidden"
           }
         >
-          <h3 className="text-4xl text-font font-semibold font-poppins px-6 py-10">
+          <h3 className="text-4xl text-font font-semibold font-poppins md:px-6 py-10">
             PORTFOLIO
           </h3>
           <div className="space-y-6 px-6">
