bd83c031e729c333572e5f0ebde7247ccc9c52a5
Deviprasad Rai P <91744010+devi5040@users.noreply.github.com>
2024-01-02 20:28:18 +0530

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,10 +9,10 @@ function Hero() {
     >
       <div className="w-[1120px] flex flex-col-reverse md:grid md:grid-cols-2">
         <div className="font-poppins flex flex-col text-white justify-center p-6 space-y-10">
-          <h1 className="text-6xl border-l-4 border-font font-bold uppercase mx-20 md:mx-0">
+          <h1 className="text-6xl border-l-4 border-font font-bold uppercase">
             &nbsp;Deviprasad &nbsp;Rai P
           </h1>
-          <h3 className="text-4xl font-semibold mx-20 md:mx-0">
+          <h3 className="text-4xl font-semibold">
             Full Stack Web Developer
           </h3>
         </div>
