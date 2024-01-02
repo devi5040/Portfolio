72e13f16a73ac0309265678402277fddd4733d67
Deviprasad Rai P <91744010+devi5040@users.noreply.github.com>
2024-01-02 20:34:10 +0530

    Update Hero.jsx

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,29 +1,72 @@
-import React from "react";
-import hero from "../assets/devii.jpg";
+import React, { useState } from "react";
+import { HashLink } from "react-router-hash-link";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 
-function Hero() {
+function Header() {
+  const [nav, setNav] = useState(true);
+  function handleNav() {
+    setNav(!nav);
+  }
   return (
-    <div
-      id="home"
-      className="w-full h-[600px] flex justify-center items-center bg-primary"
-    >
-      <div className="md:w-[1120px] flex flex-col-reverse md:grid md:grid-cols-2">
-        <div className="font-poppins flex flex-col text-white justify-center p-6 space-y-10">
-          <h1 className="text-6xl border-l-4 border-font font-bold uppercase">
-            &nbsp;Deviprasad &nbsp;Rai P
-          </h1>
-          <h3 className="text-4xl font-semibold">
-            Full Stack Web Developer
+    <div className="py-10 px-20 md:px-56 bg-primary w-full flex justify-between font-poppins border-b">
+      <h4
+        className={
+          nav ? "text-4xl cursor-pointer font-bold text-font " : "hidden"
+        }
+      >
+        PORTFOLIO
+      </h4>
+      <div>
+        <div className="text-white fixed top-8 right-8 md:hidden">
+          {nav ? (
+            <MenuIcon onClick={handleNav} />
+          ) : (
+            <CloseIcon onClick={handleNav} />
+          )}
+        </div>
+        <ul
+          className={
+            !nav
+              ? "fixed top-0 left-0 h-full w-[60%] bg-primary font-sans text-lg  text-cardFont md:hidden"
+              : "hidden"
+          }
+        >
+          <h3 className="text-4xl text-font font-semibold font-poppins px-6 py-10">
+            PORTFOLIO
           </h3>
-        </div>
-        <div className="flex justify-center md:justify-end">
-          <div className="h-[300px] w-[300px] md:h-[360px] md:w-[360px]">
-            <img src={hero} className="h-full w-full" alt="" />
+          <div className="space-y-6 px-6">
+            <li className="border-b pb-3">
+              <HashLink to="/">Home</HashLink>
+            </li>
+            <li className="border-b pb-3">
+              <HashLink to="#about">About</HashLink>
+            </li>
+            <li className="border-b pb-3">
+              <HashLink to="#skills">Skills</HashLink>
+            </li>
+            <li className="border-b pb-3">
+              <HashLink to="#projects">Projects</HashLink>
+            </li>
           </div>
-        </div>
+        </ul>
+        <ul className="font-sans text-lg space-x-8 hidden text-cardFont md:flex">
+          <li>
+            <HashLink to="/">Home</HashLink>
+          </li>
+          <li>
+            <HashLink to="#about">About</HashLink>
+          </li>
+          <li>
+            <HashLink to="#skills">Skills</HashLink>
+          </li>
+          <li>
+            <HashLink to="#projects">Projects</HashLink>
+          </li>
+        </ul>
       </div>
     </div>
   );
 }
 
-export default Hero;
+export default Header;
